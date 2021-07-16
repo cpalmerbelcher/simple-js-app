bf67dc9d765ca6ec8fb6db5535b380ed5f6b63d2
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-07-16 14:01:42 -0400

    creating objects
    adding a list of pokemon for my variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,7 @@
 let myVariable: "pokemonList";
 console.log(typeof myVariable);
+
+let pokemon1 = {name: "blubasaur", height: "7" type: "grass"};
+let pokemon1 = {name: "charmander", height: "7" type: "fire"};
+let pokemon1 = {name: "squirtle", height: "7" type: "water"};
+let pokemon1 = {name: "spearow", height: "7" type: "air"};

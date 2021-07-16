de88991ffcddd4b8ee62707a97ebbe9d626d9203
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-07-16 19:43:37 -0400

    scripts
    updated code

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,17 @@ let pokemonList = [
     name: "blubasaur",
     height: "7",
     type: ['grass', 'poison'],
-  };
+  },
   {
     name: "charmander",
     height: "6",
     type: ['fire', 'blaze'],
-  };
+  },
   {
     name: "squirtle",
     height: "5",
     type: ['water', 'swiming'],
-  };
+  },
   {
     name: "spearow",
     height: "3",

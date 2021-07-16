2c260800d35d2cf4a33d5fa288b989c599cfd94f
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-07-16 14:54:04 -0400

    updates for scripts
    editing more information for the list of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,23 @@ console.log(typeof myVariable);
 let pokemon1 = {
   name: "blubasaur",
   height: "7",
-  type: "grass"
+  type: ['grass', 'poison'],
 };
 
 let pokemon2 = {
   name: "charmander",
   height: "7",
-  type: "fire"
+  type: ['fire', 'heat'],
 };
 
 let pokemon3 = {
   name: "squirtle",
   height: "7",
-  type: "water"
+  type: ['water', 'swiming'],
 };
 
 let pokemon4 = {
   name: "spearow",
   height: "7",
-  type: "air"
+  type: ['air', 'speed'],
 };

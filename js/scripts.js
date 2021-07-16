0af4fb073fbc3620e5112509285d777290df5016
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-07-16 14:25:45 -0400

    scripts
    adjusted pokemon information and made it in list form

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 let myVariable: "pokemonList";
 console.log(typeof myVariable);
 
-let pokemon1 = {name: "blubasaur", height: "7" type: "grass"};
-let pokemon1 = {name: "charmander", height: "7" type: "fire"};
-let pokemon1 = {name: "squirtle", height: "7" type: "water"};
-let pokemon1 = {name: "spearow", height: "7" type: "air"};
+let pokemon1 = {
+  name: "blubasaur",
+  height: "7",
+  type: "grass"
+};
+
+let pokemon1 = {
+  name: "charmander",
+  height: "7",
+  type: "fire"
+};
+
+let pokemon1 = {
+  name: "squirtle",
+  height: "7",
+  type: "water"
+};
+
+let pokemon1 = {
+  name: "spearow",
+  height: "7", 
+  type: "air"
+};

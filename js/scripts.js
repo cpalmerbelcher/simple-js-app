2fe98bc12ff4cfb07588544a19cf41b74ce9a534
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-07-19 12:34:05 -0400

    scripts
    update to code for functionality

--- js/scripts.js
+++ js/scripts.js
@@ -21,13 +21,12 @@ let pokemonList = [
     height: "3",
     type: ['air', 'flying'],
   },
-];
-
+]
 for (let i = 0; i < pokemonList.height; i++){
-	if (pokemonList[i].height > 5) {
-		document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}") - Wow, that's big! </p>`);
-	} else {
-		document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}")</p>`);
-}
+  	if (pokemonList[i].height > 5) {
+  		document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}") - Wow, that's big! </p>`);
+  	} else {
+  		document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}")</p>`);
+  }
 
 }

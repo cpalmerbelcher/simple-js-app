4e766cdd73e8fcb47c4b9ff493280a67d682a219
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-07-19 11:42:51 -0400

    scripts
    adding for loop to the page to create a list of the pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,11 @@ let pokemonList = [
   },
 ];
 
+for (let i = 0; i < pokemonList.length; i++){
+	if (pokemonList[i].height > 50) {
+		document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}") - I am a tall Pokemon! </p>`);
+	} else {
+		document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}")</p>`);
+}
+
 })();

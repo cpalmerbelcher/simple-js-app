58f378af5343bc9bcacb25aa82a105f0acc1f57e
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-07-19 16:06:43 -0400

    script
    adjusting code

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,8 @@ let pokemonList = [
   },
 ]
 for (let i = 0; i < pokemonList.length; i++){
-  	console.log (pokemonList[i].height > 5) {
-  		document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big! </p>`);
-  	} else {
-  		document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height})</p>`);
-  }
+  	console.log (pokemonList[i].height > 5);
+    document.write('<p> pokemonList:height </p>')
+
 
 }

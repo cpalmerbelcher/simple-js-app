d7b313f9a762aa3b14e6dd298dfb7b8358993b20
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-07-20 14:09:17 -0400

    Update scripts.js
    updates

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ let pokedex = function () {
 
   for (let i = 0; i < pokemonList.length; i++) {
     	console.log (pokemonList[i].height > 5)
-      document.write('<p>' + pokemonList[i].name + pokemonList[i].height + Wow, that's big! '</p>');
+      document.write('<p>' + pokemonList[i].name + pokemonList[i].height + '</p>');
     }
 }
 

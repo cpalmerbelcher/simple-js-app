b09955b6fe8791547dc281411ba509de6858cd50
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-07-20 13:37:43 -0400

    script
    updated code for functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,31 @@
 let pokedex = (function () {
+  let pokemonList = [
+    {
+      name: 'blubasaur',
+      height: '7',
+      type: ['grass', 'poison'],
+    },
+    {
+      name: 'charmander',
+      height: '6',
+      type: ['fire', 'blaze'],
+    },
+    {
+      name: 'squirtle',
+      height: '5',
+      type: ['water', 'swiming'],
+    },
+    {
+      name: 'spearow',
+      height: '3',
+      type: ['air', 'flying'],
+    },
+  ];
 
-let pokemonList = [
-  {
-    name: "blubasaur",
-    height: "7",
-    type: ['grass', 'poison'],
-  },
-  {
-    name: "charmander",
-    height: "6",
-    type: ['fire', 'blaze'],
-  },
-  {
-    name: "squirtle",
-    height: "5",
-    type: ['water', 'swiming'],
-  },
-  {
-    name: "spearow",
-    height: "3",
-    type: ['air', 'flying'],
-  },
-];
-
-for (let i = 0; i < pokemonList.length; i++){
-  	console.log (pokemonList[i].height > 5);
-    document.write('<p>' + list[i].name + '</p>')
-  }
-
-pokedex ()
+  for (let i = 0; i < pokemonList.length; i++) {
+    	console.log (pokemonList[i].height > 5)
+      document.write('<p>' + list[i].name + '</p>')
+    }
 }
+
+pokedex()

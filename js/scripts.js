cd54562bf02b7ebb71e023396ee3af72fcccce01
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-08-02 12:56:17 -0400

    Update scripts.js
    show all 150 pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -23,9 +23,41 @@ let pokemonRepository = (function () {
       });
     }
 
-    function showDetails(pokemon) {
-      console.log(pokemon.name);
-    }
+    function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+  let url = item.detailsUrl;
+  return fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (details) {
+    // Now we add the details to the item
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.types = details.types;
+  }).catch(function (e) {
+    console.error(e);
+  });
+}
+
+function showDetails(pokemon) {
+loadDetails(pokemon).then(function () {
+console.log(pokemon);
+});
+}
 
     return {
       add: add,
@@ -35,7 +67,8 @@ let pokemonRepository = (function () {
   })();
 
 
-
-  pokemonRepository.getAll().forEach(function(pokemon) {
-    pokemonRepository.addListItem(pokemon);
-});
+  pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(pokemon){
+      pokemonRepository.addListItem(pokemon);
+    });
+  });

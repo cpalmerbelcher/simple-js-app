11a1e19c47f46d6ce20628ab64979c2d0b1455f5
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2021-08-02 14:34:45 -0400

    Update scripts.js
    updates

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ let pokemonRepository = (function () {
       let listpokemon = document.createElement("li");
       let button = document.createElement("button");
       button.innerText = pokemon.name;
-      button.classList.add("button-class")
+      button.classList.add("button-class");
       listpokemon.appendChild(button);
       pokemonList.appendChild(listpokemon);
       button.addEventListener('click', function(event) {
@@ -40,6 +40,7 @@ let pokemonRepository = (function () {
           detailsUrl: item.url
         };
         add(pokemon);
+        console.log(pokemon);
       });
     }).catch(function (e) {
       console.error(e);
@@ -60,19 +61,21 @@ let pokemonRepository = (function () {
     });
   }
 
-function showDetails(pokemon) {
-loadDetails(pokemon).then(function () {
-console.log(pokemon);
-});
-}
+  function showDetails(item) {
+      pokemonRepository.loadDetails(item).then(function () {
+        console.log(item);
+      });
+    }
 
     return {
-      add: add,
-      getAll: getAll,
-      addListItem: addListItem
-    };
-  })();
-
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+  };
+})();
 
   pokemonRepository.loadList().then(function() {
     pokemonRepository.getAll().forEach(function(pokemon){
